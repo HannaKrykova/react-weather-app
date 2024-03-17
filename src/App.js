2761cda06f5584293b662048033351ac85a917e2
HannaKrykova <hannakrykova@gmail.com>
2024-03-17 15:21:59 +0200

    Created footer in App.js

--- src/App.js
+++ src/App.js
@@ -3,5 +3,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by {""}
+        <a
+          href="https://github.com/HannaKrykova"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Hanna Krykova
+        </a>
+        {""} and it's {""}
+        <a
+          href="https://github.com/HannaKrykova/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
