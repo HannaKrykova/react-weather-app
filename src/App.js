5ebdd1a8ebbb90bb531571062f7f22828853a53f
HannaKrykova <hannakrykova@gmail.com>
2024-03-17 18:30:35 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
-
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was created by {""}
-        <a
-          href="https://github.com/HannaKrykova"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Hanna Krykova
-        </a>
-        {""} and it's {""}
-        <a
-          href="https://github.com/HannaKrykova/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was created by {""}
+            <a
+              href="https://github.com/HannaKrykova"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Hanna Krykova
+            </a>
+            {""} and it's {""}
+            <a
+              href="https://github.com/HannaKrykova/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
