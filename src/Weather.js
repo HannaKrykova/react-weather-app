60ad52d6eb0cdf042c45d0add2198228b72846ef
HannaKrykova <hannakrykova@gmail.com>
2024-03-17 20:59:34 +0200

    Added css styles

--- src/Weather.js
+++ src/Weather.js
@@ -4,32 +4,42 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
+      <form className="form-inline search-form">
         <div className="row">
           <div className="col-9">
             <input
+              className="form-control mr-sm-2 search-input"
               type="search"
               placeholder="Enter a city.."
-              className="search-input"
+              aria-label="Search"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <button
+              className="btn search-submit my-2 my-sm-0 w-100"
+              type="submit"
+            >
+              Search
+            </button>
           </div>
         </div>
       </form>
       <h1>Kyiv</h1>
-      <ul>
+      <ul className="forecast-details mt-3">
         <li>Sunday 18:00</li>
         <li>Rain</li>
       </ul>
-      <div className="row">
+      <div className="row mt-4 mb-4">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="rainy"
-          />
-          9°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+              alt="rainy"
+            />
+            <span className="temperature">9</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
