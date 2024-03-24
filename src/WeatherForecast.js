cad23d3e498e36ad4f6f1e0785ef4d25c92130d6
HannaKrykova <hannakrykova@gmail.com>
2024-03-24 16:42:43 +0200

    Created the forecast for 6 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,6 +17,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
           <div className="col">
             <WeatherForecastDay data={forecast[0]} />
           </div>
